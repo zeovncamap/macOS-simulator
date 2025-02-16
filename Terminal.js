09e0494f00d992e4a7f4dca64b58bca20f765ee8
zeovncamap <zeovncamap@gmail.com>
2025-02-16 15:31:00 +0700

    Create Terminal.js

---
+++ Terminal.js
@@ -0,0 +1,10 @@
+function Terminal() {
+  return (
+    <div className="bg-black text-white p-4 w-80 h-80 border border-gray-300 rounded-lg shadow-lg">
+      <h2 className="text-xl font-bold">Terminal</h2>
+      <pre className="mt-4">Welcome to the terminal!</pre>
+    </div>
+  );
+}
+
+export default Terminal;
