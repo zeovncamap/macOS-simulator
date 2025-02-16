1b5cb098c81f0b7daa3ca822e629c9b1479bcdf1
zeovncamap <zeovncamap@gmail.com>
2025-02-16 15:30:36 +0700

    Create Safari.js

---
+++ Safari.js
@@ -0,0 +1,10 @@
+function Safari() {
+  return (
+    <div className="bg-white p-4 w-80 h-80 border border-gray-300 rounded-lg shadow-lg">
+      <h2 className="text-xl font-bold">Safari</h2>
+      <p className="mt-4">Your favorite browser!</p>
+    </div>
+  );
+}
+
+export default Safari;
